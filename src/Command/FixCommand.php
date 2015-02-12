db8647d075d5fd6eb608cd15feb12e17d46bb13f
Martin Procházka <juniwalk@outlook.cz>
2015-02-12 22:18:11 +0100

    ADD: Fix command

---
+++ src/Command/FixCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   Darwin
+ * @link      https://github.com/juniwalk/darwin
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Darwin\Command;
+
+use Symfony\Component\Console\Helper\DescriptorHelper;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FixCommand extends Command
+{
+    /**
+     * Configure this command.
+     */
+    protected function configure()
+    {
+        $this->setName('fix');
+        $this->setDescription('Fix permissions of the files and dirs.');
+
+        //
+        $this->addArgument('name', InputArgument::OPTIONAL, 'Path to the project.');
+        $this->addOption('owner', 'o', InputOption::VALUE_OPTIONAL, 'Define owner for files.');
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+        if ($name) {
+            $text = 'Hello '.$name;
+        } else {
+            $text = 'Hello';
+        }
+
+        if ($input->getOption('yell')) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+    }
+}
