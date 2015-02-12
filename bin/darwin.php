e20eb165dabe78c28c526712f9f1d940eb3c6633
Martin Procházka <juniwalk@outlook.cz>
2015-02-12 17:58:18 +0100

    ADD: Binary file

---
+++ bin/darwin.php
@@ -0,0 +1,17 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   Darwin
+ * @link      https://github.com/juniwalk/darwin
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Darwin\Bin;
+
+
+// Include vendor autoloader to access projects
+include __DIR__.'/../../vendor/autoload.php';
+
