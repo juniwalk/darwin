b222b88472096305d4f2779ee2b36bd0f31055aa
Martin Procházka <juniwalk@outlook.cz>
2015-02-12 23:54:29 +0100

    FIX: Resolve output interface

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -11,6 +11,7 @@
 namespace JuniWalk\Darwin\Command;
 
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class Command extends \Symfony\Component\Console\Command\Command
 {
