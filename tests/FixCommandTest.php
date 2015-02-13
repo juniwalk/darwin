c999699a55954ce7ab55e6cc4f34f6e410d26a14
Martin Procházka <juniwalk@outlook.cz>
2015-02-13 22:41:16 +0100

    ADD: First test

---
+++ tests/FixCommandTest.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   Darwin
+ * @link      https://github.com/juniwalk/darwin
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+namespace JuniWalk\Darwin\Tests;
+
+use JuniWalk\Darwin\Darwin;
+
+class FixCommandTest extends \PHPUnit_Framework_TestCase
+{
+}
