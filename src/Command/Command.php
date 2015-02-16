21d8446e5c67150267473100e7c9ea55f48ba0d2
Martin Procházka <juniwalk@outlook.cz>
2015-02-16 21:30:24 +0100

    DEL: New line destructor, not working

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -34,16 +34,6 @@ class Command extends \Symfony\Component\Console\Command\Command
 
 
     /**
-     * Called when command finishes.
-     */
-    public function __destruct()
-    {
-        // Move pointer to new line
-        $this->write(PHP_EOL);
-    }
-
-
-    /**
      * Dialog to confirm some action.
      *
      * @param InputInterface   $input   Input stream
