9b1cf956fb6389849170711900bf4b2b4e1ee210
Martin Procházka <juniwalk@outlook.cz>
2017-10-12 10:08:58 +0200

    Added folder argument & config option

--- src/Command/FilePermissionCommand.php
+++ src/Command/FilePermissionCommand.php
@@ -11,17 +11,55 @@
 namespace JuniWalk\Darwin\Command;
 
 use JuniWalk\Darwin\Tools\ProgressIterator;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Finder\Finder;
 
 final class FilePermissionCommand extends \Symfony\Component\Console\Command\Command
 {
+	/**
+	 * Working directory for the permission fixing.
+	 * @var string
+	 */
+	private $folder;
+
+	/**
+	 * List of rules for permission fixer.
+	 * @var Rule[]
+	 */
+	private $rules;
+
+
+	/**
+	 * @return string
+	 */
+	public function getFolder() : string
+	{
+		return $this->folder ?: getcwd();
+	}
+
+
 	protected function configure()
 	{
 		$this->setDescription('Fix file permissions in given directory');
 		$this->setName('file:permission')->setAliases(['fix']);
+
+		$this->addArgument('folder', InputArgument::OPTIONAL, 'Working directory for permission fixer');
+		$this->addOption('config', 'c', InputOption::VALUE_REQUIRED, 'Name of configuration file', 'default');
+	}
+
+
+	/**
+	 * @param  InputInterface   $input
+	 * @param  OutputInterface  $output
+	 */
+	protected function initialize(InputInterface $input, OutputInterface $output)
+	{
+		$this->rules = $this->loadRules($input->getOption('config'));
+		$this->folder = $input->getArgument('folder');
 	}
 
 
@@ -50,8 +88,7 @@ final class FilePermissionCommand extends \Symfony\Component\Console\Command\Com
 	 */
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
-		$finder = (new Finder)
-			->in($folder = getcwd())
+		$finder = (new Finder)->in($this->getFolder())
 			->exclude('vendor')
 			->exclude('bin');
 
@@ -60,8 +97,8 @@ final class FilePermissionCommand extends \Symfony\Component\Console\Command\Com
 		}
 
 		$progress = new ProgressIterator($output, $finder);
-		$progress->onSingleStep[] = function ($bar, $file) use ($rules, $folder) {
-			$bar->setMessage(str_replace($folder, '.', $file));
+		$progress->onSingleStep[] = function ($bar, $file) use ($rules) {
+			$bar->setMessage(str_replace($this->getFolder(), '.', $file));
 
 			foreach ($rules as $rule) {
 				$rule->apply($file);
@@ -75,12 +112,13 @@ final class FilePermissionCommand extends \Symfony\Component\Console\Command\Com
 
 
 	/**
+	 * @param  string  $file
 	 * @return Rule[]
 	 */
-	private function loadRules()
+	private function loadRules(string $file)
 	{
 		$config = $this->getHelper('config')
-			->load('fix.neon');
+			->load($file.'.neon');
 
 		$class = $config['className'];
 		$rules = [];
